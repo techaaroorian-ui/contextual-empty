e2e33da02aed3d705ebb83932d6f01ecddeaabf9
Janarthanan Soundhararajan <janarthanan1821993@gmail.com>
2026-08-17 00:23:13 +0530

    chore: explicitly set happy-dom environment in test script

--- packages/contextual-empty/vitiest.config.ts
+++ packages/contextual-empty/vitiest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "jsdom", // Simulates a browser environment
+    environment: "happy-dom", // Simulates a browser environment
     setupFiles: ["./vitest.setup.ts"], // Runs before our tests
     globals: true,
   },
